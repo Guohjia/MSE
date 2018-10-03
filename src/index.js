2b9f10016e6b890b7612efdd63859b79e7352474
guohjia <guohjia@gmail.com>
2018-10-03 21:55:42 +0800

    new git test

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ var myPlayer = new Player({
     url:'./MP4file/frag_bunny.mp4',
     video:document.getElementById('mseVideo'),
     mimeCodec:'video/mp4;codecs="avc1.42E01E,mp4a.40.2"',
-    bufferLimited:8
+    bufferLimited:7
 })
